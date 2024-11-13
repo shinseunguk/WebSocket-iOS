0f39c15b054bcc869e696257bed5f7fed412f989
신승욱 <ukseung.dev@incross.com>
2024-11-13 17:03:20 +0900

    Initial Commit

---
+++ WebSocket-Example/WebSocket_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocket_ExampleApp.swift
+//  WebSocket-Example
+//
+//  Created by ukseung.dev on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WebSocket_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
